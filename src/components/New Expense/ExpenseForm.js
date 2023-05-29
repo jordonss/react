1840fb8103f6676ea1e694a2af23833eddf2561f
jordonss <nemezg@gmail.com>
2023-05-29 22:02:11 +0300

    slozhno

--- src/components/New Expense/ExpenseForm.js
+++ src/components/New Expense/ExpenseForm.js
@@ -18,34 +18,37 @@ const ExpenseForm = (props) => {
     setEnteredDate(event.target.value);
   };
 
-	const submitHandler = (event) => {
-		event.preventDefault();
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-		const expenseData = {
-			title: enteredTitle,
-			amount: enteredAmount,
-			date: new Date(enteredDate)
-		}
+    const expenseData = {
+      title: enteredTitle,
+      amount: +enteredAmount,
+      date: new Date(enteredDate),
+    };
 
-		props.onSaveExpenseData(expenseData);
-		setEnteredTitle('');
-		setEnteredAmount('');
-		setEnteredDate('');
-
-	}
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
 
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
-						value={enteredAmount}
+            value={enteredAmount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -55,7 +58,7 @@ const ExpenseForm = (props) => {
           <label>Date</label>
           <input
             type="date"
-						valur={enteredDate}
+            valur={enteredDate}
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
@@ -63,6 +66,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
